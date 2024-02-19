b5a88bce4918136eb0702b54bc0ab21db534db61
jannatshova <153752136+jannatshova@users.noreply.github.com>
2024-02-19 10:15:11 +0600

    landing page updated
    this file has been updated . now we can select file what we want to share.

--- main.py
+++ main.py
@@ -10,6 +10,51 @@ root.geometry("450x560+500+200")
 root.configure(bg="#f4fdfe")
 root.resizable(False,False)
 
+def Send():
+    window=Toplevel(root)
+    window.title("Send")
+    window.geometry('450x560+500+200')
+    window.configure(bg="#f4fdfe")
+    window.resizable(False,False)
+
+    def select_file():
+        filename=filedialog.askopenfilename(initialdir=os.getcwd(),
+                                            title='Select Image File',
+                                            filetype=(('file type','*.txt'),('all files','*.*')))
+
+
+    #icon
+    image_icon1=PhotoImage(file="images/send-50.png")
+    window.iconphoto(False,image_icon1)
+
+    Sbackground=PhotoImage(file="images/sender.png")
+    Label(window,image=Sbackground).place(x=-2,y=0)
+
+    Mbackground=PhotoImage(file="images/id.png")
+    Label(window,image=Mbackground,bg=("#f4fdfe")).place(x=100,y=260)
+
+    host=socket.gethostname()
+    Label(window,text=f'ID:{host}',bg='white',fg='black').place(x=140,y=290)
+
+    Button(window,text="+ select file",width=10,height=1,font='arial 14 bold',bg="#fff",fg="#000",command=select_file).place(x=160,y=150)
+    Button(window,text="SEND",width=8,height=1,font='arial 14 bold',bg="#fff",fg="#000").place(x=300,y=150)
+
+    window.mainloop()
+
+def Receive():
+    main = Toplevel(root)
+    main.title("Receive")
+    main.geometry('450x560+500+200')
+    main.configure(bg="#f4fdfe")
+    main.resizable(False,False)
+
+    # icon
+    image_icon1 = PhotoImage(file="images/ireceive-64.png")
+    main.iconphoto(False,image_icon1)
+
+    main.mainloop()
+
+
 
 
 #icon
@@ -20,7 +65,7 @@ Label(root,text="File Transfer",font=('Acumin Variable Concept',20,'bold'),bg="#
 Frame(root,width=400,height=2,bg="#f3f5f6").place(x=25,y=80)
 
 send_image=PhotoImage(file="images/send-50.png")
-send=Button(root,image=send_image,bg="#f4fdfe",bd=0)
+send=Button(root,image=send_image,bg="#f4fdfe",bd=0,command=Send)
 send.place(x=50,y=100)
 
 receive_image=PhotoImage(file="images/ireceive-64.png")
