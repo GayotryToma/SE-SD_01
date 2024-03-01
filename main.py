075b0ef07109171608aa84804c858f8eb6fba256
jannatshova <153752136+jannatshova@users.noreply.github.com>
2024-03-01 12:54:23 +0600

    landing file updated
    now we can select any file what we want to share and send it to receiver. if there is no connection with user then there will be an output saying can't find any connections.

--- main.py
+++ main.py
@@ -18,9 +18,25 @@ def Send():
     window.resizable(False,False)
 
     def select_file():
+        global filename
         filename=filedialog.askopenfilename(initialdir=os.getcwd(),
                                             title='Select Image File',
                                             filetype=(('file type','*.txt'),('all files','*.*')))
+    def sender():
+        s=socket.socket()
+        host=socket.gethostname()
+        port=8080
+        s.bind((host,port))
+        s.listen(1)
+        print(host)
+        print('waiting for any incoming connections.... ')
+        conn,addr=s.accept()
+        file=open(filename,'rb')
+        file_data=file.read(1024)
+        conn.send(file_data)
+        print("Data has been transmitted successfully")
+
+
 
 
     #icon
@@ -36,26 +52,24 @@ def Send():
     host=socket.gethostname()
     Label(window,text=f'ID:{host}',bg='white',fg='black').place(x=140,y=290)
 
-    Button(window,text="+ select file",width=10,height=1,font='arial 14 bold',bg="#fff",fg="#000",command=select_file).place(x=160,y=150)
-    Button(window,text="SEND",width=8,height=1,font='arial 14 bold',bg="#fff",fg="#000").place(x=300,y=150)
+    Button(window, text="+ select file", width=10, height=1, font='arial 14 bold', bg="#fff", fg="#000", command=select_file).place(x=160, y=150)
+    Button(window,text="SEND",width=8,height=1,font='arial 14 bold',bg="#000",fg="#fff",command=sender).place(x=300,y=150)
 
     window.mainloop()
 
 def Receive():
-    main = Toplevel(root)
+    main=Toplevel(root)
     main.title("Receive")
     main.geometry('450x560+500+200')
     main.configure(bg="#f4fdfe")
-    main.resizable(False,False)
+    main.resizable(False, False)
 
-    # icon
+
+    #icon
     image_icon1 = PhotoImage(file="images/ireceive-64.png")
     main.iconphoto(False,image_icon1)
 
     main.mainloop()
-
-
-
 
 #icon
 image_icon=PhotoImage(file="images/file-64.png")
