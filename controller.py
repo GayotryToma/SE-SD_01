59a6b8f336cf573cac42c35be7b3ba39f7e61bab
jannatshova <153752136+jannatshova@users.noreply.github.com>
2024-03-23 04:47:48 +0600

    follwed architecture followed
    this part control both model.py and view.py .

---
+++ controller.py
@@ -0,0 +1,15 @@
+# controller.py
+from view import FileTransferView
+from model import FileTransferModel
+
+
+class FileTransferController:
+    def __init__(self, view, model):
+        self.view = view
+        self.model = model
+        self.view.run()
+
+if __name__ == "__main__":
+    model = FileTransferModel()
+    view = FileTransferView()
+    controller = FileTransferController(view, model)
