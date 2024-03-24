bfa807344de3a44831ea5a31493f59fa0bb9aa65
jannatshova <153752136+jannatshova@users.noreply.github.com>
2024-03-25 03:11:33 +0600

    model part is modified
    we declared port only once and we extended the file size.

--- model.py
+++ model.py
@@ -1,40 +1,57 @@
 import socket
-
+from tkinter import filedialog,Tk
+import os
 
 class FileTransferModel:
-    CHUNK_SIZE = 10485760  # Adjust chunk size according to needs
+    CHUNK_SIZE = 10485760# Adjust chunk size according to needs
+    PORT=8080
 
-    def send_file(self, filename):
-        s = socket.socket()
-        host = socket.gethostname()
-        port = 8080
-        s.bind((host, port))
+    def select_file(self):
+        root=Tk()
+        root.withdraw() #hide the main window
+
+        file_path=filedialog.askopenfilename(title="Select File To Send")
+        print("Selected file path:",file_path)
+        root.destroy()
+        return file_path
+
+    def send_file(self,filename,file_data,chunk_data):
+       ## filename=self.select_file()
+        #if not filename:
+         #   print("No file selected.Exiting...")
+          #  return
+
+        s=socket.socket()
+        host=socket.gethostname()
+        s.bind((host,self.PORT))
         s.listen(1)
         print(host)
-        print('waiting for any incoming connections.... ')
-        conn, addr = s.accept()
+        print('waiting for any incomig connections...')
+        conn,addr=s.accept()
 
-        with open(filename, 'rb') as file:
-            while True:
-                file_data = file.read(self.CHUNK_SIZE)
-                if not file_data:
-                    break  # Reached end of file
 
-                conn.send(file_data)
+        with open(filename,'wb') as file:
+                file.write(file_data)
 
         print("Data has been transmitted successfully")
 
-    def receive_file(self, sender_id, filename):
-        s = socket.socket()
-        port = 8080
-        s.connect((sender_id, port))
+    def receive_file(self,sender_id,filename):
+        s=socket.socket()
+        s.connect((sender_id,self.PORT))
 
-        with open(filename, 'wb') as file:
+
+        with open(filename,'wb') as file:
             while True:
-                file_data = s.recv(self.CHUNK_SIZE)
+                file_data=s.recv(self.CHUNK_SIZE)
                 if not file_data:
-                    break  # Reached end of transmission
-
+                    break
                 file.write(file_data)
 
         print("File has been received successfully")
+
+if __name__ == "__main__":
+   model = FileTransferModel()
+   model.send_file()
+
+
+
