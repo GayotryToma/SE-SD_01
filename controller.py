39e9f55a527c499a03063610254ea298961d6995
jannatshova <153752136+jannatshova@users.noreply.github.com>
2024-04-29 10:21:03 +0600

    Add files via upload
    some change has been made to send a receive a file

--- controller.py
+++ controller.py
@@ -10,8 +10,17 @@ class FileTransferController:
         self.model = model
         self.view.controller=self
         self.view.login_button.config(command=self.login)
+        #self.view.select_file_button.config(command=self.select_file)
         self.view.run()
         self.main_view =None
+
+
+    #def select_file(self):
+        # Implement file selection logic here
+        #filename = self.view.select_file()
+        #if filename:
+            #print("Selected file:", filename)
+            # Optionally, you can update the view with the selected filename
     def handle_login_success(self):
         print("handling login success")
         self.main_view= FileTransferView(mode="Main")
